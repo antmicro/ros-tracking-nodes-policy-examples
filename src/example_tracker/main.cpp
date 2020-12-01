06d2989627bc3e5baa12f43c2279c03e94e69781
Marcin Szmagara <mszmagara@internships.antmicro.com>
2020-12-01 13:15:25 +0100

    make example_policy aware of resolution

--- src/example_tracker/main.cpp
+++ src/example_tracker/main.cpp
@@ -8,36 +8,37 @@
 
 ros::Publisher bbox_pub;
 
-void updateTester(unsigned num)
+void updateTester(const tracking_tester::frame_msg& frame)
 {
     // uncomment to simulate a slow algorithm
     // usleep(3e5);
+    unsigned num = frame.number;
     tracking_tester::optional_bbox_msg message;
-	static unsigned calls{};
-    message.bbox.x = 440 + 25 * std::sin(calls / 30.0);
-    message.bbox.y = 230 + 25 * std::cos(calls / 30.0);
+    static unsigned calls{};
     message.bbox.width = 70;
     message.bbox.height = 50;
+    message.bbox.x = frame.image.width / 2 - message.bbox.width / 2 + 25 * std::sin(calls / 30.0);
+    message.bbox.y = frame.image.height / 2 - message.bbox.height / 2 + 25 * std::cos(calls / 30.0);
     message.timestamp = ros::Time::now();
     message.valid = true;
-	message.number = num;
+    message.number = num;
 
-	bbox_pub.publish(message);
-	calls++;
+    bbox_pub.publish(message);
+    calls++;
 }
 
 void fetchData(const tracking_tester::frame_msg& frame)
 {
-	updateTester(frame.number);
+    updateTester(frame);
 }
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "example_tracker");
-	ROS_INFO("Initialized!");
-	ros::NodeHandle nh;
-	
-	auto frame_sub = nh.subscribe("tracking_tester/frame", 1, fetchData);
-	bbox_pub = nh.advertise<tracking_tester::optional_bbox_msg>("tracking_tester/bbox", 1, false);
-	ros::spin();
+    ros::init(argc, argv, "example_tracker");
+    ROS_INFO("Initialized!");
+    ros::NodeHandle nh;
+
+    auto frame_sub = nh.subscribe("tracking_tester/frame", 1, fetchData);
+    bbox_pub = nh.advertise<tracking_tester::optional_bbox_msg>("tracking_tester/bbox", 1, false);
+    ros::spin();
 }
